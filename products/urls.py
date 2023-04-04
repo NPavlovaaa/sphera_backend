e1448b84a379f3e1cf04a82ebe6309251316dc0c
NPavlovaaa <n.pavlova.web@gmail.com>
2023-04-04 23:00:46 +0300

    fixed products.urls

--- products/urls.py
+++ products/urls.py
@@ -6,7 +6,7 @@ router = routers.DefaultRouter()
 router.register(r'products', ProductListViewSet)
 router.register(r'roasting', RoastingMethodsViewSet)
 router.register(r'processing', ProcessingMethodsViewSet)
-router.register(r'variety', VarietySerializer)
+router.register(r'variety', VarietyViewSet)
 router.register(r'weight_selection', WeightSelectionViewSet)
 router.register(r'weight', WeightViewSet)
 
