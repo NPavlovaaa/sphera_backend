87d462648831eb7754f49a3dee3ec0ba84795dd1
NPavlovaaa <n.pavlova.web@gmail.com>
2023-05-16 15:20:35 +0300

    fixed favorites views and urls

--- clients/urls.py
+++ clients/urls.py
@@ -5,7 +5,7 @@ from clients.views import *
 router = routers.DefaultRouter()
 router.register(r'clients', ClientViewSet)
 router.register(r'carts', CartViewSet)
-router.register(r'favorites', FavoriteViewSet)
+# router.register(r'favorites', FavoriteViewSet)
 
 
 urlpatterns = [
@@ -14,6 +14,11 @@ urlpatterns = [
     path('cart_in_orders/', CartOrdersView.as_view(), name=''),
     path('admin_carts/', CartAdminView.as_view(), name=''),
     path('cart_addition/', CartView.as_view(), name=''),
-    path('favorite/<int:id>/', FavoriteView.as_view(), name=''),
+    path('favorites/', FavoriteListView.as_view()),
+    path('create_favorite/', FavoriteListView.as_view()),
+    path('favorite_detail/<int:product>/', FavoriteDetailView.as_view()),
+    path('favorite_delete/<int:id>/', FavoriteDetailView.as_view()),
+    # path('favorite/<int:product>/', FavoriteView.as_view(), name=''),
+    # path('favorite_list/', FavoriteListView.as_view(), name=''),
     path('product_cart/<int:product>/<int:weight_selection>/', ProductInCartView.as_view(), name=''),
 ]
--- clients/views.py
+++ clients/views.py
@@ -1,17 +1,16 @@
-import jwt
+from rest_framework.generics import get_object_or_404
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.viewsets import ModelViewSet
 from clients.models import Client, Cart, Favorite
 from clients.serializer import ClientSerializer, CartSerializer, FavoriteSerializer
 from django.db import transaction
-from rest_framework import status
+from rest_framework import status, viewsets
 
 from products.models import ProcessingMethod, Product, RoastingMethod, Weight, WeightSelection
 from products.serializer import ProcessingMethodSerializer, ProductSerializer, RoastingMethodSerializer, \
     WeightSelectionSerializer, WeightSerializer
 from rest_framework.parsers import MultiPartParser, FormParser, JSONParser
-from rest_framework.generics import RetrieveAPIView, ListAPIView
 from rest_framework import permissions
 
 
@@ -64,7 +63,6 @@ class CartView(APIView):
             cart_data = {'weight_selection': request.data['weight_selection'], 'client': client.client_id}
             serializer_cart = CartSerializer(data=cart_data)
             if serializer_cart.is_valid():
-                
                 with transaction.atomic():
                     Cart.objects.create(
                         client=client,
@@ -76,6 +74,7 @@ class CartView(APIView):
                     )
                     return Response(serializer_cart.data, status=status.HTTP_201_CREATED)
             return Response(serializer_cart.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
 class CartOrdersView(APIView):
     """ Список подкорзин клиента в заказе
@@ -115,6 +114,7 @@ class CartOrdersView(APIView):
                           'order': cart_serializer.data['order']})
         return Response(carts)
 
+
 class CartViewSet(ModelViewSet):
     parser_classes = (MultiPartParser, FormParser, JSONParser)
     queryset = Cart.objects.all()
@@ -139,26 +139,58 @@ class ProductInCartView(APIView):
         return Response(data)
 
 
-class FavoriteViewSet(ModelViewSet):
-    parser_classes = (MultiPartParser, FormParser, JSONParser)
-    queryset = Favorite.objects.all()
-    serializer_class = FavoriteSerializer
+class FavoriteListView(APIView):
+    """
+    APIView for listing or retrieving favorites list
+    """
+
+    permission_classes = [permissions.IsAuthenticated]
+    model = Favorite
+
+    def get(self, request):
+        client = Client.objects.get(user=self.request.user.user_id)
+        queryset = Favorite.objects.filter(client=client.client_id)
+        data = []
+        for item in queryset:
+            product = Product.objects.get(product_id=item.product.product_id)
+            product_serializer = ProductSerializer(product)
+            data.append(product_serializer.data)
+        return Response(data)
+
+    def post(self, request):
+        client = Client.objects.get(user=self.request.user.user_id)
+        product = Product.objects.get(product_id=request.data['product'])
+        favorite, created = self.model.objects.get_or_create(client=client, product=product)
+        serializer = FavoriteSerializer(favorite)
+        if not created:
+            favorite.delete()
+            return Response(serializer.data, status=status.HTTP_204_NO_CONTENT)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
-class FavoriteView(APIView):
-    parser_classes = (MultiPartParser, FormParser, JSONParser)
+class FavoriteDetailView(APIView):
+    """
+    APIView for listing, retrieving or deleting favorites item
+    """
 
-    def get(self, id):
-        favorite = Favorite.objects.filter(client=id)
-        favorites = []
-        for item in favorite:
-            favorite_serializer = FavoriteSerializer(item)
-            favorites.append(favorite_serializer.data)
-        return Response(favorites)
+    permission_classes = [permissions.IsAuthenticated]
+    model = Favorite
+
+    def get(self, request, product):
+        client = Client.objects.get(user=self.request.user.user_id)
+        favorite = get_object_or_404(Favorite.objects.all(), client=client.client_id, product=product)
+        serializer = FavoriteSerializer(favorite)
+        return Response(serializer.data)
+
+    def delete(self, request, id):
+        favorite = get_object_or_404(Favorite.objects.all(), favorite_id=id)
+        favorite.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 
 class CartAdminView(APIView):
     parser_classes = (MultiPartParser, FormParser, JSONParser)
+    permission_classes = [permissions.IsAuthenticated]
 
     def get(self, request):
         data = Cart.objects.filter(active=False)
